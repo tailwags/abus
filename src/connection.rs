551ff3bdba32e5d090b4b0cc266573032f7ac7f7
Matilde Morrone <contact@matilde.pet>
2026-04-02 15:43:11 +0200

    MIgrate to anchovy

--- src/connection.rs
+++ src/connection.rs
@@ -1,32 +1,21 @@
 use std::{
-    collections::VecDeque,
     fmt::Display,
-    io::{IoSlice, IoSliceMut},
-    mem::MaybeUninit,
-    os::{
-        fd::{AsFd, BorrowedFd, OwnedFd},
-        unix::net::UnixStream,
-    },
     pin::Pin,
-    task::{Context, Poll, ready},
+    task::{Context, Poll},
 };
 
-use rustix::{
-    net::{
-        RecvAncillaryBuffer, RecvAncillaryMessage, RecvFlags, SendAncillaryBuffer,
-        SendAncillaryMessage, SendFlags, recvmsg, sendmsg,
-    },
-    process::getuid,
-};
-use tokio::io::{
-    self, AsyncBufReadExt as _, AsyncRead, AsyncWrite, AsyncWriteExt as _, BufReader, ReadBuf,
-    unix::AsyncFd,
-};
 use crate::tracing::{error, info};
+use anchovy::AnchovyStream;
+use pin_project_lite::pin_project;
+use rustix::process::getuid;
+use tokio::{
+    io::{
+        self, AsyncBufReadExt as _, AsyncRead, AsyncWrite, AsyncWriteExt as _, BufReader, ReadBuf,
+    },
+    net::UnixStream,
+};
 
 use crate::utils::HexU32;
-
-const MAX_FDS_PER_MSG: usize = 253;
 
 enum State {
     #[allow(unused)]
@@ -47,49 +36,26 @@ impl Display for State {
     }
 }
 
-pub struct Connection {
-    stream: BufReader<StreamWrapper>,
+pin_project! {
+    pub struct Connection {
+        #[pin]
+        stream: BufReader<AnchovyStream>,
+        server_guid: String,
+        unix_fd_passing: bool,
+    }
 }
 
 impl Connection {
     pub async fn new() -> io::Result<Self> {
-        let stream = tokio::net::UnixStream::connect("/run/dbus/system_bus_socket").await?;
+        let stream = UnixStream::connect("/run/dbus/system_bus_socket").await?;
 
         info!("Connected to dbus system socket");
 
-        let stream = StreamWrapper::authenticate(stream.into_std()?).await?;
-
-        Ok(Self { stream })
+        Self::authenticate(stream).await
     }
 
-    pub fn server_guid(&self) -> &str {
-        &self.stream.get_ref().server_guid
-    }
-}
-
-struct StreamWrapper {
-    stream: AsyncFd<UnixStream>,
-    server_guid: String,
-    unix_fd_passing: bool,
-    pub decode_fds: VecDeque<OwnedFd>,
-    pub encode_fds: VecDeque<OwnedFd>,
-}
-
-impl StreamWrapper {
-    pub async fn authenticate(stream: UnixStream) -> io::Result<BufReader<Self>> {
-        let this = Self {
-            stream: AsyncFd::new(stream)?,
-            server_guid: String::new(),
-            unix_fd_passing: false,
-            decode_fds: VecDeque::new(),
-            encode_fds: VecDeque::new(),
-        };
-
-        this._authenticate().await
-    }
-
-    async fn _authenticate(self) -> io::Result<BufReader<Self>> {
-        let mut stream = BufReader::new(self);
+    async fn authenticate(stream: UnixStream) -> io::Result<Self> {
+        let mut stream = BufReader::new(AnchovyStream::new(stream)?);
 
         let uid = HexU32::new(getuid().as_raw());
 
@@ -100,6 +66,8 @@ impl StreamWrapper {
             .await?;
 
         let mut state = State::WaitingForOK;
+        let mut server_guid = String::new();
+        let mut unix_fd_passing = false;
 
         let mut line_buffer = String::new();
 
@@ -126,7 +94,7 @@ impl StreamWrapper {
                 }
 
                 (State::WaitingForData | State::WaitingForOK, "OK") => {
-                    stream.get_mut().server_guid = arg.to_string();
+                    server_guid = arg.to_string();
                     stream.write_all(b"NEGOTIATE_UNIX_FD\r\n").await?;
 
                     State::WaitingForAgreeUnixFD
@@ -147,12 +115,12 @@ impl StreamWrapper {
                 }
 
                 (State::WaitingForAgreeUnixFD, "AGREE_UNIX_FD") => {
-                    stream.get_mut().unix_fd_passing = true;
+                    unix_fd_passing = true;
                     stream.write_all(b"BEGIN\r\n").await?;
                     break;
                 }
                 (State::WaitingForAgreeUnixFD, "ERROR") => {
-                    stream.get_mut().unix_fd_passing = false;
+                    unix_fd_passing = false;
                     stream.write_all(b"BEGIN\r\n").await?;
                     break;
                 }
@@ -173,134 +141,46 @@ impl StreamWrapper {
             line_buffer.clear();
         }
 
-        Ok(stream)
+        Ok(Self {
+            stream,
+            server_guid,
+            unix_fd_passing,
+        })
     }
 
-    fn poll_write_impl(
-        &mut self,
-        cx: &mut Context<'_>,
-        bufs: &[IoSlice<'_>],
-    ) -> Poll<io::Result<usize>> {
-        let stream = &mut self.stream;
-        let encode_fds = &mut self.encode_fds;
+    pub fn server_guid(&self) -> &str {
+        &self.server_guid
+    }
 
-        loop {
-            let mut guard = ready!(stream.poll_write_ready(cx))?;
-
-            let send_result = {
-                let raw: Vec<BorrowedFd<'_>> = encode_fds.iter().map(|fd| fd.as_fd()).collect();
-
-                let mut cmsg_space =
-                    [MaybeUninit::uninit(); rustix::cmsg_space!(ScmRights(MAX_FDS_PER_MSG))];
-                let mut ancillary = SendAncillaryBuffer::new(&mut cmsg_space);
-
-                if !raw.is_empty() {
-                    ancillary.push(SendAncillaryMessage::ScmRights(&raw));
-                }
-
-                guard.try_io(|inner| {
-                    sendmsg(
-                        inner.get_ref(),
-                        bufs,
-                        &mut ancillary,
-                        SendFlags::DONTWAIT | SendFlags::NOSIGNAL,
-                    )
-                    .map_err(|e| io::Error::from_raw_os_error(e.raw_os_error()))
-                })
-            };
-
-            match send_result {
-                Ok(Ok(msg)) => {
-                    encode_fds.clear();
-                    return Poll::Ready(Ok(msg));
-                }
-                Ok(Err(err)) => {
-                    return Poll::Ready(Err(err));
-                }
-
-                Err(_would_block) => continue,
-            }
-        }
+    pub fn unix_fd_passing(&self) -> bool {
+        self.unix_fd_passing
     }
 }
 
-impl AsyncRead for StreamWrapper {
+impl AsyncRead for Connection {
     fn poll_read(
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
-    ) -> Poll<io::Result<()>> {
-        let this = self.get_mut();
-
-        let stream = &mut this.stream;
-        let decode_fds = &mut this.decode_fds;
-
-        loop {
-            let mut guard = ready!(stream.poll_read_ready(cx))?;
-
-            let mut cmsg_space =
-                [MaybeUninit::uninit(); rustix::cmsg_space!(ScmRights(MAX_FDS_PER_MSG))];
-            let mut ancillary = RecvAncillaryBuffer::new(&mut cmsg_space);
-
-            let unfilled = buf.initialize_unfilled();
-
-            match guard.try_io(|inner| {
-                recvmsg(
-                    inner.get_ref(),
-                    &mut [IoSliceMut::new(unfilled)],
-                    &mut ancillary,
-                    RecvFlags::DONTWAIT | RecvFlags::CMSG_CLOEXEC,
-                )
-                .map_err(|e| io::Error::from_raw_os_error(e.raw_os_error()))
-            }) {
-                Ok(Ok(msg)) => {
-                    for message in ancillary.drain() {
-                        if let RecvAncillaryMessage::ScmRights(fds) = message {
-                            for fd in fds {
-                                decode_fds.push_back(fd);
-                            }
-                        }
-                    }
-                    buf.advance(msg.bytes);
-                    return Poll::Ready(Ok(()));
-                }
-                Ok(Err(err)) => return Poll::Ready(Err(err)),
-                Err(_would_block) => continue,
-            }
-        }
+    ) -> Poll<std::io::Result<()>> {
+        self.project().stream.poll_read(cx, buf)
     }
 }
 
-impl AsyncWrite for StreamWrapper {
+impl AsyncWrite for Connection {
     fn poll_write(
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &[u8],
-    ) -> Poll<io::Result<usize>> {
-        self.get_mut().poll_write_impl(cx, &[IoSlice::new(buf)])
+    ) -> Poll<std::io::Result<usize>> {
+        self.project().stream.poll_write(cx, buf)
     }
 
-    fn poll_write_vectored(
-        self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        bufs: &[IoSlice<'_>],
-    ) -> Poll<io::Result<usize>> {
-        self.get_mut().poll_write_impl(cx, bufs)
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        self.project().stream.poll_flush(cx)
     }
 
-    fn is_write_vectored(&self) -> bool {
-        true
-    }
-
-    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        Poll::Ready(Ok(()))
-    }
-
-    fn poll_shutdown(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        self.get_mut()
-            .stream
-            .get_ref()
-            .shutdown(std::net::Shutdown::Write)?;
-        Poll::Ready(Ok(()))
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        self.project().stream.poll_shutdown(cx)
     }
 }
