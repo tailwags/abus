d686688dc98315c41c121297ddb0fdc11b17ab48
Matilde Morrone <contact@matilde.pet>
2026-08-06 17:19:25 +0200

    Refactor endianness helpers into a macro

--- crates/abus/src/endianness.rs
+++ crates/abus/src/endianness.rs
@@ -26,117 +26,46 @@ impl TryFrom<u8> for Endianness {
     }
 }
 
+macro_rules! endianness {
+    ($ty:ty, $get:ident, $get_le:ident, $put:ident, $put_le:ident, $from_bytes:ident, $to_bytes:ident) => {
+        pub(crate) const fn $from_bytes(&self, bytes: [u8; std::mem::size_of::<$ty>()]) -> $ty {
+            match self {
+                Endianness::LittleEndian => <$ty>::from_le_bytes(bytes),
+                Endianness::BigEndian => <$ty>::from_be_bytes(bytes),
+            }
+        }
+
+        pub(crate) const fn $to_bytes(&self, val: $ty) -> [u8; std::mem::size_of::<$ty>()] {
+            match self {
+                Endianness::LittleEndian => val.to_le_bytes(),
+                Endianness::BigEndian => val.to_be_bytes(),
+            }
+        }
+
+        pub(crate) fn $get<B: Buf>(&self, buf: &mut B) -> $ty {
+            match self {
+                Endianness::LittleEndian => buf.$get_le(),
+                Endianness::BigEndian => buf.$get(),
+            }
+        }
+
+        pub(crate) fn $put<B: BufMut>(&self, buf: &mut B, val: $ty) {
+            match self {
+                Endianness::LittleEndian => buf.$put_le(val),
+                Endianness::BigEndian => buf.$put(val),
+            }
+        }
+    };
+}
+
+#[rustfmt::skip]
 #[allow(dead_code)]
 impl Endianness {
-    pub(crate) fn u32_from_bytes(&self, bytes: [u8; 4]) -> u32 {
-        match self {
-            Endianness::LittleEndian => u32::from_le_bytes(bytes),
-            Endianness::BigEndian => u32::from_be_bytes(bytes),
-        }
-    }
-
-    pub(crate) fn u32_to_bytes(&self, val: u32) -> [u8; 4] {
-        match self {
-            Endianness::LittleEndian => val.to_le_bytes(),
-            Endianness::BigEndian => val.to_be_bytes(),
-        }
-    }
-
-    pub(crate) fn get_u16<B: Buf>(&self, buf: &mut B) -> u16 {
-        match self {
-            Endianness::LittleEndian => buf.get_u16_le(),
-            Endianness::BigEndian => buf.get_u16(),
-        }
-    }
-
-    pub(crate) fn get_i16<B: Buf>(&self, buf: &mut B) -> i16 {
-        match self {
-            Endianness::LittleEndian => buf.get_i16_le(),
-            Endianness::BigEndian => buf.get_i16(),
-        }
-    }
-
-    pub(crate) fn get_u32<B: Buf>(&self, buf: &mut B) -> u32 {
-        match self {
-            Endianness::LittleEndian => buf.get_u32_le(),
-            Endianness::BigEndian => buf.get_u32(),
-        }
-    }
-
-    pub(crate) fn get_i32<B: Buf>(&self, buf: &mut B) -> i32 {
-        match self {
-            Endianness::LittleEndian => buf.get_i32_le(),
-            Endianness::BigEndian => buf.get_i32(),
-        }
-    }
-
-    pub(crate) fn get_u64<B: Buf>(&self, buf: &mut B) -> u64 {
-        match self {
-            Endianness::LittleEndian => buf.get_u64_le(),
-            Endianness::BigEndian => buf.get_u64(),
-        }
-    }
-
-    pub(crate) fn get_i64<B: Buf>(&self, buf: &mut B) -> i64 {
-        match self {
-            Endianness::LittleEndian => buf.get_i64_le(),
-            Endianness::BigEndian => buf.get_i64(),
-        }
-    }
-
-    pub(crate) fn get_f64<B: Buf>(&self, buf: &mut B) -> f64 {
-        match self {
-            Endianness::LittleEndian => buf.get_f64_le(),
-            Endianness::BigEndian => buf.get_f64(),
-        }
-    }
-
-    pub(crate) fn put_u16<B: BufMut>(&self, buf: &mut B, val: u16) {
-        match self {
-            Endianness::LittleEndian => buf.put_u16_le(val),
-            Endianness::BigEndian => buf.put_u16(val),
-        }
-    }
-
-    pub(crate) fn put_i16<B: BufMut>(&self, buf: &mut B, val: i16) {
-        match self {
-            Endianness::LittleEndian => buf.put_i16_le(val),
-            Endianness::BigEndian => buf.put_i16(val),
-        }
-    }
-
-    pub(crate) fn put_u32<B: BufMut>(&self, buf: &mut B, val: u32) {
-        match self {
-            Endianness::LittleEndian => buf.put_u32_le(val),
-            Endianness::BigEndian => buf.put_u32(val),
-        }
-    }
-
-    pub(crate) fn put_i32<B: BufMut>(&self, buf: &mut B, val: i32) {
-        match self {
-            Endianness::LittleEndian => buf.put_i32_le(val),
-            Endianness::BigEndian => buf.put_i32(val),
-        }
-    }
-
-    pub(crate) fn put_u64<B: BufMut>(&self, buf: &mut B, val: u64) {
-        match self {
-            Endianness::LittleEndian => buf.put_u64_le(val),
-            Endianness::BigEndian => buf.put_u64(val),
-        }
-    }
-
-    pub(crate) fn put_i64<B: BufMut>(&self, buf: &mut B, val: i64) {
-        match self {
-            Endianness::LittleEndian => buf.put_i64_le(val),
-            Endianness::BigEndian => buf.put_i64(val),
-        }
-    }
-
-    pub(crate) fn put_f64<B: BufMut>(&self, buf: &mut B, val: f64) {
-        match self {
-            Endianness::LittleEndian => buf.put_f64_le(val),
-            Endianness::BigEndian => buf.put_f64(val),
-        }
-    }
+    endianness!(u16, get_u16, get_u16_le, put_u16, put_u16_le, u16_from_bytes, u16_to_bytes);
+    endianness!(i16, get_i16, get_i16_le, put_i16, put_i16_le, i16_from_bytes, i16_to_bytes);
+    endianness!(u32, get_u32, get_u32_le, put_u32, put_u32_le, u32_from_bytes, u32_to_bytes);
+    endianness!(i32, get_i32, get_i32_le, put_i32, put_i32_le, i32_from_bytes, i32_to_bytes);
+    endianness!(u64, get_u64, get_u64_le, put_u64, put_u64_le, u64_from_bytes, u64_to_bytes);
+    endianness!(i64, get_i64, get_i64_le, put_i64, put_i64_le, i64_from_bytes, i64_to_bytes);
+    endianness!(f64, get_f64, get_f64_le, put_f64, put_f64_le, f64_from_bytes, f64_to_bytes);
 }
